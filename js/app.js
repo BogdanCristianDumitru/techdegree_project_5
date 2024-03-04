651ef08990a0865a1a653fe1972e3aebd785d159
Bogdan <bogdan_dumitru_85@yahoo.com>
2024-03-04 10:02:38 -0500

    search function added

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+//const search = new Filter('search', 'data-caption');
+
+const search = document.getElementById('search');
+
+//Event Listener for Search
+search.addEventListener('keyup', (e) => {
+    let currentValue= e.target.value.toLowerCase();
+    
+    const galleryPhotos = document.querySelectorAll('.gallery-photo');
+    
+    galleryPhotos.forEach(photo => {
+        const caption = photo.getAttribute('data-caption');
+        
+        if (caption.toLowerCase().includes(currentValue)) {
+            photo.style.display = 'block';
+        } else {
+            photo.style.display = 'none';
+        }
+    });
+});
+
+
+
+
+
+
